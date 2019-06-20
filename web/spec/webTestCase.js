624c8492126673ce4d58c92d0af04942f7ca5575
Gordon Krull <gkrull@pivotal.io>
2019-06-20 12:06:26 +0700

    First failing web test

--- web/spec/webTestCase.js
+++ web/spec/webTestCase.js
@@ -1,5 +1,21 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
+import React from 'react'
+import * as ReactDOM from 'react-dom'
+import RPSApp from '../src/RPSApp'
+
+describe("play form", function () {
+    describe('when the play use case tells the UI that the input is invalid', function () {
+        it('tells the user that their input is invalid', function () {
+            let domFixture = document.createElement('div')
+            document.body.appendChild(domFixture)
+
+            let alwaysInvalidRequest = {
+                play: (player1Throw, player2Throw, result) => result.invalid()
+            }
+
+            ReactDOM.render(<RPSApp requests={alwaysInvalidRequest} />, domFixture)
+            expect(domFixture.innerText).not.toContain('INVALID!')
+            document.querySelector('button').click()
+            expect(domFixture.innerText).toContain('INVALID!')
+        })
     })
 })
