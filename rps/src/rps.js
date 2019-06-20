585ef387df95273afc2418e2466162fad3820272
Gordon Krull <gkrull@pivotal.io>
2019-06-20 10:31:46 +0700

    Create PlayMatchRequest object

--- rps/src/rps.js
+++ rps/src/rps.js
@@ -1,19 +1,25 @@
 function Match() {
-    this.playMatch = (player1Throw, player2Throw, observer) => {
+    this.playMatch = (player1Throw, player2Throw, result) => {
+        new PlayMatchRequest(player1Throw, player2Throw, result).process();
+    }
+}
+
+function PlayMatchRequest(player1Throw, player2Throw, result) {
+    this.process = () => {
         if (!['rock', 'paper', 'scissors'].includes(player1Throw) ||
             !['rock', 'paper', 'scissors'].includes(player2Throw)) {
-            observer.invalid();
+            result.invalid();
         }
         else if (player1Throw === player2Throw) {
-            observer.draw();
+            result.draw();
         }
         else if (player1Throw === 'rock' && player2Throw === 'scissors' ||
             player1Throw === 'scissors' && player2Throw === 'paper' ||
             player1Throw === 'paper' && player2Throw === 'rock') {
-            observer.player1Wins();
+            result.player1Wins();
 
         } else {
-            observer.player2Wins();
+            result.player2Wins();
         }
     }
 }
